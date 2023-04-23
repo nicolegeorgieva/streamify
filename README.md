f62230da052f14e729e21f7201dcf83e1e20976a
Nicole <nicole.georgievva@gmail.com>
2023-04-23 20:36:48 +0300

    Setup CameraX + recording

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Streamify
+
+## Technologies:
+
+- Jetpack Compose
+- Accompanist Permissions
+- CameraX
+- ExoPlayer
+
+### Sources:
+
+- https://betterprogramming.pub/build-a-camera-android-app-in-jetpack-compose-using-camerax-4d5dfbfbe8ec
--- app/src/main/java/com/example/streamify/MainActivity.kt
+++ app/src/main/java/com/example/streamify/MainActivity.kt
@@ -4,40 +4,48 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.streamify.ui.theme.StreamifyTheme
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.streamify.Route.VIDEO_PREVIEW_ARG
+import com.example.streamify.ui.theme.CameraAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            StreamifyTheme {
-                // A surface container using the 'background' color from the theme
+            CameraAppTheme {
+                val navController = rememberNavController()
+
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    NavHost(
+                        navController = navController,
+                        startDestination = Route.VIDEO
+                    ) {
+                        composable(Route.VIDEO) {
+                            VideoCaptureScreen(navController = navController)
+                        }
+
+                        composable(Route.VIDEO_PREVIEW_FULL_ROUTE) {
+                            val uri = it.arguments?.getString(VIDEO_PREVIEW_ARG) ?: ""
+                            VideoPreviewScreen(uri = uri)
+                        }
+                    }
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    StreamifyTheme {
-        Greeting("Android")
-    }
+object Route {
+    const val VIDEO = "video"
+    const val VIDEO_PREVIEW_FULL_ROUTE = "video_preview/{uri}"
+    const val VIDEO_PREVIEW = "video_preview"
+    const val VIDEO_PREVIEW_ARG = "uri"
 }
