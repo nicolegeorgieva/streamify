3ef7f549183ed26248042dd924e817b40ebd26aa
Nicole <nicole.georgievva@gmail.com>
2023-04-24 15:57:36 +0300

    Refactor CameraWrapper.kt and add comments

--- app/src/main/java/com/example/streamify/CameraWrapper.kt
+++ app/src/main/java/com/example/streamify/CameraWrapper.kt
@@ -17,35 +17,41 @@ import java.util.concurrent.Executor
 import kotlin.coroutines.resume
 import kotlin.coroutines.suspendCoroutine
 
+// Obtain the camera provider asynchronously
 suspend fun Context.getCameraProvider(): ProcessCameraProvider = suspendCoroutine { continuation ->
     ProcessCameraProvider.getInstance(this).also { future ->
         future.addListener(
             {
                 continuation.resume(future.get())
             },
-            ContextCompat.getMainExecutor(this) // Use mainExecutor from the context
+            ContextCompat.getMainExecutor(this) // Get main executor from the context
         )
     }
 }
 
+// Create a Preview instance with the given PreviewView's surface provider
 suspend fun Context.createPreview(previewView: PreviewView): Preview = Preview.Builder()
     .build()
     .apply { setSurfaceProvider(previewView.surfaceProvider) }
 
+// Define a quality selector for the video recording
 fun createQualitySelector(): QualitySelector = QualitySelector.from(
     Quality.FHD,
     FallbackStrategy.lowerQualityOrHigherThan(Quality.FHD)
 )
 
+// Create a Recorder instance with the given executor and quality selector
 fun createRecorder(mainExecutor: Executor, qualitySelector: QualitySelector): Recorder =
     Recorder.Builder()
         .setExecutor(mainExecutor)
         .setQualitySelector(qualitySelector)
         .build()
 
+// Create a VideoCapture instance with the given Recorder instance as its output
 fun createVideoCapture(recorder: Recorder): VideoCapture<Recorder> =
     VideoCapture.withOutput(recorder)
 
+// Bind the camera provider to the lifecycle and use cases
 suspend fun Context.bindUseCases(
     lifecycleOwner: LifecycleOwner,
     cameraSelector: CameraSelector,
@@ -62,12 +68,13 @@ suspend fun Context.bindUseCases(
     )
 }
 
+// Set up the VideoCapture use case with the given lifecycle owner, camera selector, and preview view
 suspend fun Context.createVideoCaptureUseCase(
     lifecycleOwner: LifecycleOwner,
     cameraSelector: CameraSelector,
     previewView: PreviewView
 ): VideoCapture<Recorder> {
-    val mainExecutor = ContextCompat.getMainExecutor(this) // Use mainExecutor from the context
+    val mainExecutor = ContextCompat.getMainExecutor(this) // Get main executor from the context
     val preview = createPreview(previewView)
     val qualitySelector = createQualitySelector()
     val recorder = createRecorder(mainExecutor, qualitySelector)
@@ -78,11 +85,13 @@ suspend fun Context.createVideoCaptureUseCase(
     return videoCapture
 }
 
+// Create a video file with the given filename format and output directory
 fun createVideoFile(filenameFormat: String, outputDirectory: File): File = File(
     outputDirectory,
     SimpleDateFormat(filenameFormat, Locale.US).format(System.currentTimeMillis()) + ".mp4"
 )
 
+// Prepare the recording with the given context, video capture instance, and video file
 fun prepareRecording(
     context: Context,
     videoCapture: VideoCapture<Recorder>,
@@ -90,6 +99,7 @@ fun prepareRecording(
 ): PendingRecording = videoCapture.output
     .prepareRecording(context, FileOutputOptions.Builder(videoFile).build())
 
+// Start the video recording with the given parameters and consumer
 @SuppressLint("MissingPermission")
 fun startRecordingVideo(
     context: Context,
