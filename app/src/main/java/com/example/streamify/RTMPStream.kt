9aec2d170d4fb614dea6d9562fb2899efbfc1497
Nicole <nicole.georgievva@gmail.com>
2023-04-24 22:05:58 +0300

    Setup Twitch streaming

--- app/src/main/java/com/example/streamify/RTMPStream.kt
+++ app/src/main/java/com/example/streamify/RTMPStream.kt
@@ -1,31 +1,40 @@
 package com.example.streamify
 
 
+import android.util.Log
 import com.pedro.rtmp.rtmp.RtmpClient
 import com.pedro.rtmp.utils.ConnectCheckerRtmp
 
-private const val STREAMING_URL = ""
+private const val TWITCH_KEY = ""
+private const val STREAMING_URL = "rtmp://live.twitch.tv/app/$TWITCH_KEY"
 
 val rtmpClient = RtmpClient(object : ConnectCheckerRtmp {
     override fun onAuthErrorRtmp() {
+        Log.i("stream", "onAuthErrorRtmp")
     }
 
     override fun onAuthSuccessRtmp() {
+        Log.i("stream", "onAuthSuccessRtmp")
     }
 
     override fun onConnectionFailedRtmp(reason: String) {
+        Log.i("stream", "onConnectionFailedRtmp: $reason")
     }
 
     override fun onConnectionStartedRtmp(rtmpUrl: String) {
+        Log.i("stream", "onConnectionStartedRtmp: $rtmpUrl")
     }
 
     override fun onConnectionSuccessRtmp() {
+        Log.i("stream", "onConnectionSuccessRtmp")
     }
 
     override fun onDisconnectRtmp() {
+        Log.i("stream", "onDisconnectRtmp")
     }
 
     override fun onNewBitrateRtmp(bitrate: Long) {
+        Log.i("stream", "onNewBitrateRtmp: $bitrate")
     }
 })
 
