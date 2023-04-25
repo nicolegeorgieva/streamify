7349fde9a406cfdb5150bdb88a2841e9159b681b
Nicole <nicole.georgievva@gmail.com>
2023-04-25 18:11:13 +0300

    Add share video button on VideoPreviewScreen.kt

--- app/src/main/java/com/example/streamify/MainActivity.kt
+++ app/src/main/java/com/example/streamify/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.streamify
 
+import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -7,6 +8,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -34,7 +36,10 @@ class MainActivity : ComponentActivity() {
 
                         composable(Route.VIDEO_PREVIEW_FULL_ROUTE) {
                             val uri = it.arguments?.getString(VIDEO_PREVIEW_ARG) ?: ""
-                            VideoPreviewScreen(uri = uri)
+                            val context = LocalContext.current
+                            VideoPreviewScreen(uri = uri, onShareClick = {
+                                createShareIntent(context, Uri.parse(uri))
+                            })
                         }
                     }
                 }
--- app/src/main/java/com/example/streamify/VideoPreviewScreen.kt
+++ app/src/main/java/com/example/streamify/VideoPreviewScreen.kt
@@ -1,12 +1,19 @@
 package com.example.streamify
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import com.google.android.exoplayer2.ExoPlayer
 import com.google.android.exoplayer2.MediaItem
@@ -18,7 +25,8 @@ import com.google.android.exoplayer2.ui.StyledPlayerView
  */
 @Composable
 fun VideoPreviewScreen(
-    uri: String
+    uri: String,
+    onShareClick: () -> Unit
 ) {
     val context = LocalContext.current
 
@@ -46,5 +54,25 @@ fun VideoPreviewScreen(
         onDispose {
             exoPlayer.release()
         }
+    }
+
+    Spacer(modifier = Modifier.padding(top = 16.dp))
+
+    IconButton(
+        onClick = onShareClick
+    ) {
+        Icon(
+            painter = painterResource(R.drawable.baseline_share_24),
+            contentDescription = "",
+            modifier = Modifier.size(52.dp),
+            tint = Color.White
+        )
+    }
+
+    Button(
+        onClick = onShareClick,
+        modifier = Modifier.padding(top = 16.dp)
+    ) {
+        Text(text = stringResource(id = R.string.share_video))
     }
 }
