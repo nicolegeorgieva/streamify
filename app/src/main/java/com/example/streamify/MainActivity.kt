4a25b9c48f43035a4b7897272a85d29a1f9c2bf5
Nicole <nicole.georgievva@gmail.com>
2023-04-26 17:37:52 +0300

    Refactor

--- app/src/main/java/com/example/streamify/MainActivity.kt
+++ app/src/main/java/com/example/streamify/MainActivity.kt
@@ -53,7 +53,7 @@ class MainActivity : ComponentActivity() {
 
 object Route {
     const val VIDEO = "video"
-    const val VIDEO_PREVIEW_FULL_ROUTE = "video_preview/{uri}"
     const val VIDEO_PREVIEW = "video_preview"
     const val VIDEO_PREVIEW_ARG = "uri"
+    const val VIDEO_PREVIEW_FULL_ROUTE = "$VIDEO_PREVIEW/{$VIDEO_PREVIEW_ARG}"
 }
